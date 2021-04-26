861377e4b348add990c9f6c750131248feeb3adc
Fatima <fcamero12@gmail.com>
2021-04-26 15:06:56 -0700

    Create models and migrate

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Project(models.Model):
+    """Model representing project which will contain list of tasks"""
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, blank=True, null=True)
+
+
+class Tag(models.Model):
+    """Model representing tag"""
+    name = models.CharField(max_length=20)
+
+
+class Task(models.Model):
+   title = models.CharField(max_length=200)
+   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+   
+   class Priority(models.IntegerChoices):
+       LOW = 1
+       MEDIUM = 2
+       HIGH = 3
+   priority = models.IntegerField(choices=Priority.choices, default=Priority.LOW)
+   
+   due_date = models.DateField()
+   note = models.TextField(blank=True, null=True)
+   tag = models.ManyToManyField(Tag)
+   status = models.BooleanField(default=False)
